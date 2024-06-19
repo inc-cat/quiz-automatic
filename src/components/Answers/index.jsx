9e68cc30d1f878f4828e1246e728d54997887bdb
Anwar Louis <smmaniac@hotmail.co.uk>
2024-06-19 20:18:57 +0100

    Initialise Answer component

--- src/components/Answers/index.jsx
+++ src/components/Answers/index.jsx
@@ -1,15 +1,45 @@
 import questions from '../../assets/questions.json';
 import answerData from '../../assets/answer_data.json';
 import { useState } from 'react';
+import { Bar, Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
 
 function Answers() {
-  let [question, setQuestion] = useState();
+  let [question, setQuestion] = useState(0);
   const names = Object.keys(answerData.outcome_data);
   console.log(names);
+
+  const nextQuestion = function () {
+    setQuestion((question += 1));
+  };
+
+  let scoreData = {
+    labels: names,
+    datasets: [
+      {
+        label: 'Scores',
+        data: names.map(function (name) {
+          return answerData.outcome_data[name][question];
+        }),
+      },
+    ],
+  };
 
   return (
     <>
       <h1>Answers page!</h1>
+      <Bar data={scoreData} />
+      <button onClick={nextQuestion}>Next</button>
     </>
   );
 }
